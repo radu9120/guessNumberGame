4e52897d5c15c375670b83e96051e160b187a264
radu9120 <chircaradu9120@gmail.com>
2022-10-10 19:41:23 +0100

    updates

--- starter/script.js
+++ starter/script.js
@@ -7,7 +7,37 @@
 
 // document.querySelector('.guess').value = 23;
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number 🥲';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = ' 🎉Correct Number!';
+    //Guess above secret number with loss implementation!
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' Number to High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 🥶';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    //Guess lower than secret number with loss implmentation!
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Number to Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 🥶';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
