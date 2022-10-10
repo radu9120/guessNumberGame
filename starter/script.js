db1ca434ade3e52c7f3c16298d22dfe0869426d0
radu9120 <chircaradu9120@gmail.com>
2022-10-10 21:51:28 +0100

    nearly there

--- starter/script.js
+++ starter/script.js
@@ -9,6 +9,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -19,11 +20,19 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number 🥲';
 
     //When a player wins
+  } else if (guess > 20) {
+    document.querySelector('.message').textContent = 'Not allowed number🥲';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = ' 🎉Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      textContent = highScore;
+    }
+
     //Guess above secret number with loss implementation!
   } else if (guess > secretNumber) {
     if (score > 1) {
